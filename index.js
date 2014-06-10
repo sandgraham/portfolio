f9ae8f22290ad6b0c661f985d57c6b0ee08118a4
Graham Murdoch <grahammurdoch8@gmail.com>
2014-06-10 15:39:03 -0700

    went with fake button model for a static single page

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 $(function(){
 
-	$('.trigger').click(function(event){
-		event.preventDefault();
+	$('.trigger').click(function(e){
 		$(this).next().slideToggle();
 	});
 
+	$('.button').click(function(e){
+		nav(e.target.id);
+	});
+
+	var openContent = "main-content";
+	function nav (clickedButton) {
+		var clickedContent = clickedButton.split("-")[0] + "-content";
+		if (clickedContent !== openContent) {
+			$('#'+openContent).hide();
+			$('#'+clickedContent).show();
+			openContent = clickedContent;
+		};
+	};
+	
 })
